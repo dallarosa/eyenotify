5f7ea4d5d969fff59e433888a08e5eb840371862
Francisco Dalla Rosa Soares <francisco_d_r_soares@cyberagent.co.jp>
2014-01-06 19:29:41 +0900

    WIP

--- eyenotify_darwin.go
+++ eyenotify_darwin.go
@@ -13,26 +13,31 @@ func processEvent() {
 }
 
 func runKqueue() {
-	fd, err := syscall.Kqueue()
-	evTrackList := make([]syscall.Kevent_t, 0)
+	kq, err := syscall.Kqueue()
 	if err != nil {
 		log.Fatal("error initializing Kqueue: ", err)
 		return
 	}
 
-	addFilesToKqueue(evTrackList, path)
+	evTrackList := []syscall.Kevent_t{}
+
+	addFilesToKqueue(&evTrackList, path)
 
 	// configure timeout
-	timeout := syscall.Timespec{
-		Sec:  0,
-		Nsec: 0,
-	}
+	//	timeout := syscall.Timespec{
+	//		Sec:  60,
+	//		Nsec: 0,
+	//	}
+	events := []syscall.Kevent_t{}
+	log.Print(evTrackList)
 
 	// wait for events
 	for {
 		// create kevent
-		events := make([]syscall.Kevent_t, 10)
-		_, err := syscall.Kevent(fd, evTrackList, events, &timeout)
+
+		_, err := syscall.Kevent(kq, evTrackList, events, nil)
+		//		log.Print("I should be blocking")
+		//		log.Print(events)
 		if err != nil {
 			debug.PrintStack()
 			log.Fatal("Error creating kevent: ", err)
@@ -44,7 +49,7 @@ func runKqueue() {
 	}
 }
 
-func addFilesToKqueue(evTrackList []syscall.Kevent_t, dirPath string) {
+func addFilesToKqueue(evTrackList *[]syscall.Kevent_t, dirPath string) {
 	fileList, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		log.Fatal("ReadDir failed: ", err)
@@ -58,6 +63,8 @@ func addFilesToKqueue(evTrackList []syscall.Kevent_t, dirPath string) {
 			if len(strings.Split(fileName, ".")) > 1 {
 				fileExt := strings.Split(fileName, ".")[1]
 				if fileExt == ext {
+					log.Print("newPath: ", newPath)
+					log.Print("fileName: ", fileName)
 					fd, err := syscall.Open(newPath, syscall.O_RDONLY, 0)
 					if err != nil {
 						log.Fatal("Open failed: ", err)
@@ -66,12 +73,11 @@ func addFilesToKqueue(evTrackList []syscall.Kevent_t, dirPath string) {
 					ev := syscall.Kevent_t{
 						Ident:  uint64(fd),
 						Filter: syscall.EVFILT_VNODE,
-						Flags:  syscall.EV_ADD | syscall.EV_ENABLE | syscall.EV_ONESHOT,
-						Fflags: syscall.NOTE_DELETE | syscall.NOTE_WRITE,
-						Data:   0,
-						Udata:  nil,
+						Flags:  syscall.EV_ADD | syscall.EV_ENABLE,
+						Fflags: syscall.NOTE_EXTEND | syscall.NOTE_WRITE | syscall.NOTE_RENAME | syscall.NOTE_DELETE,
 					}
-					evTrackList = append(evTrackList, ev)
+					*evTrackList = append(*evTrackList, ev)
+					log.Print(*evTrackList)
 				}
 			}
 		}
